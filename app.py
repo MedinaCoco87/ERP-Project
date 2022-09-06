bdb35390e4ea7c8c0697d6fe97639e7357c14bd0
Cristian Medina <cristiandaniel.med@gmail.com>
2022-09-05 21:35:00 -0300

    Early version of the API with first routes

--- app.py
+++ app.py
@@ -1,4 +1,24 @@
-from flask import flask
+import os
 
+from cs50 import SQL
+from flask import Flask, jsonify, redirect, request
+from flask import Flask, flash, jsonify, redirect, render_template, request, session
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+
+
+# Configuring flask app
 app = Flask(__name__)
 
+# Configure database
+db = SQL("sqlite:///erpdatabase.db")
+
+@app.route ("/user", methods = ["POST"])
+def create_user():
+    user = request.get_json()
+    db.execute ("INSERT INTO users (username, hashed_pass, profile, status) VALUES (?, ?, ?, ?)", user["username"], user["password"], user["profile"], user["status"])
+    return jsonify({"message": "user created"})
+
+@app.route ("/user/<user_id>", methods = ["GET"])
+def get_user_by_id():
+    pass
