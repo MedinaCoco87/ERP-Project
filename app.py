13f2df16c2d1128b871218ec2745892d66bfaaba
Cristian Medina <cristiandaniel.med@gmail.com>
2022-09-06 10:24:13 -0300

    Add some more routes and the file .gitignore

--- app.py
+++ app.py
@@ -13,12 +13,26 @@ app = Flask(__name__)
 # Configure database
 db = SQL("sqlite:///erpdatabase.db")
 
+# Initial routes to test from postman
+
 @app.route ("/user", methods = ["POST"])
 def create_user():
     user = request.get_json()
     db.execute ("INSERT INTO users (username, hashed_pass, profile, status) VALUES (?, ?, ?, ?)", user["username"], user["password"], user["profile"], user["status"])
     return jsonify({"message": "user created"})
 
+
+@app.route ("/user", methods = ["GET"])
+def get_all_users():
+    users = db.execute("SELECT * FROM users")
+    if not users:
+        return jsonify({"message": "user not found"}), 400
+    return jsonify(users), 200
+    
+
 @app.route ("/user/<user_id>", methods = ["GET"])
-def get_user_by_id():
-    pass
+def get_user_by_id(user_id):
+    user = db.execute("SELECT * FROM users WHERE id = ?", user_id)
+    if not user:
+        return jsonify({"message": "user not found"}), 400
+    return jsonify(user), 200
