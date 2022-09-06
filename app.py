7fd8a3ad4a8fd12f171fb322b332efb8257ed2a8
Cristian Medina <cristiandaniel.med@gmail.com>
2022-09-06 10:55:58 -0300

    Add route to create a customer. Testing pending.

--- app.py
+++ app.py
@@ -15,24 +15,39 @@ db = SQL("sqlite:///erpdatabase.db")
 
 # Initial routes to test from postman
 
-@app.route ("/user", methods = ["POST"])
+@app.route ("/users", methods = ["POST"])
 def create_user():
     user = request.get_json()
     db.execute ("INSERT INTO users (username, hashed_pass, profile, status) VALUES (?, ?, ?, ?)", user["username"], user["password"], user["profile"], user["status"])
     return jsonify({"message": "user created"})
 
 
-@app.route ("/user", methods = ["GET"])
+@app.route ("/users", methods = ["GET"])
 def get_all_users():
     users = db.execute("SELECT * FROM users")
     if not users:
         return jsonify({"message": "user not found"}), 400
     return jsonify(users), 200
-    
 
-@app.route ("/user/<user_id>", methods = ["GET"])
+
+@app.route ("/users/<user_id>", methods = ["GET"])
 def get_user_by_id(user_id):
     user = db.execute("SELECT * FROM users WHERE id = ?", user_id)
     if not user:
         return jsonify({"message": "user not found"}), 400
     return jsonify(user), 200
+
+# I intentionally choose not to allow deletion of users to avoid messing up with databases.
+
+@app.route ("/customers", methods = ["POST"])
+def create_customer():
+    customer = request.get_json()
+    db.execute(
+        "INSERT INTO customers (company_name, tax_id, address_street, address_city, \
+                address_country, payment_condition, credit_line) VALUES (?, ?, ?, ?, ?, ?, ?)", 
+                customer["company_name"], customer["tax_id"], customer["address_street"], 
+                customer["address_city"], customer["address_country"], customer["payment_condition"],
+                customer["credit_line"]
+                )
+    return jsonify({"message": "customer created"})
+
