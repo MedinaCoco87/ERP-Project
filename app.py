1a0751d41392ea124e2cb4ede493e8e247ff1f20
Cristian Medina <cristiandaniel.med@gmail.com>
2022-09-06 17:50:11 -0300

    Add routes to get customer by ID and name alike

--- app.py
+++ app.py
@@ -10,6 +10,9 @@ from werkzeug.security import check_password_hash, generate_password_hash
 # Configuring flask app
 app = Flask(__name__)
 
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
 # Configure database
 db = SQL("sqlite:///erpdatabase.db")
 
@@ -58,7 +61,23 @@ def update_customer(customer_id):
     for key in customer_new_data:
         db.execute("UPDATE customers SET ? = ? WHERE id = ?", key, customer_new_data[key], customer_id)
 
-    return jsonify({"message": "user updated"})
+    return jsonify({"message": "user updated"}), 200
+
+@app.route("/customers/<customer_id>", methods = ["GET"])
+def get_customer_by_id(customer_id):
+    customer = db.execute("SELECT * FROM customers WHERE id = ?", customer_id)
+    if not customer:
+        return jsonify({"message": "customer not found"}), 400
+    return jsonify(customer), 200
+    
+
+@app.route("/customers/", methods = ["GET"])
+def get_customer_by_name():
+    get_the_name = request.get_json()
+    customers = db.execute("SELECT * FROM customers WHERE company_name LIKE ?", ('%'+get_the_name["company_name"]+'%'))
+    if not customers:
+        return jsonify({"message": "customer not found"}), 400
+    return jsonify(customers), 200
 
 
 # Database is validating category_id is an integer between 100 and 999.
@@ -67,5 +86,9 @@ def create_category():
     category = request.get_json()
     db.execute("INSERT INTO item_categories (id, description) VALUES (?, ?)", category["id"], category["description"].upper())
     return jsonify({"message": "item_category created"})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
 
