215d6926f07afca248605bc5ec837bd2e2151233
Cristian Medina <cristiandaniel.med@gmail.com>
2022-09-09 12:01:55 -0300

    Add route to create items. Change item_categories. Add a counter column to relate item_id with its category

--- app.py
+++ app.py
@@ -89,6 +89,24 @@ def create_category():
     db.execute("INSERT INTO item_categories (id, description, created_by) VALUES (?, ?, ?)", category["id"], category["description"].upper(), category["created_by"])
     return jsonify({"message": "item_category created"})
 
+@app.route ("/items", methods = ["POST"])
+def create_item():
+    # Get json from postman
+    item = request.get_json()
+    # Identify the category for the new item and get the category full row from its table.
+    item_category_row = db.execute("SELECT * FROM item_categories WHERE id = ?", item["category_id"])
+    # Get the counter from the category
+    category_counter = item_category_row[0]["counter"]
+    # Generate the item_id by concatenating category_id + category_id_counter
+    item_id = int(str(item["category_id"]) + str(category_counter).zfill(4))
+    # Insert new item into items table.
+    db.execute("INSERT INTO items (id, description, full_description, category_id, status, created_by) VALUES (?, ?, ?, ?, ?, ?)", item_id, item["description"], item["full_description"], item["category_id"], item["status"], item["created_by"])
+    # Update counter on item_categories
+    category_counter = category_counter + 1
+    db.execute("UPDATE item_categories SET counter = ? WHERE id = ?", category_counter, item["category_id"])
+    return jsonify({"message": "item created", "item": item_id}), 200
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
