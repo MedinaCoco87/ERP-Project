055e9d987d725fad25d8a62da26b3dbbdc4835fb
Cristian Medina <cristiandaniel.med@gmail.com>
2022-09-13 12:23:35 -0300

    Add route to create quotes. Updates tables body and header.

--- app.py
+++ app.py
@@ -96,13 +96,13 @@ def get_category(item_category):
     category = db.execute("SELECT * FROM item_categories WHERE id = ?", item_category)
     return jsonify(category), 200
 
+
 @app.route ("/get_all_categories", methods = ["GET"])
 def get_all_categories():
     categories = db.execute("SELECT * FROM item_categories")
     if not categories:
         return jsonify({"message": "there are no records in categories yet"}), 400
     return jsonify(categories), 200
-
 
 
 
@@ -140,6 +140,37 @@ def get_items_by_category(category_id):
     if not items:
         return jsonify({"message": "this category is empty"}), 400
     return jsonify(items), 200
+
+@app.route ("/create_quote", methods = ["POST"])
+def create_quote():
+    # Get the full json data
+    full_quote = request.get_json()
+    # Separate header from body
+    quote_header = full_quote["quote_header"]
+    quote_body = full_quote["quote_body"]
+    # Update first headers table to generate the quote number
+    db.execute ("INSERT INTO quote_header (customer_id, created_by) VALUES (?, ?)", quote_header["customer_id"], quote_header["created_by"])
+    # Get the quote number to use it in quote body
+    last_header = db.execute("SELECT * FROM quote_header ORDER BY quote_num DESC LIMIT 1")
+    quote_number = last_header[0]["quote_num"]
+    # Loop through all lines of quote body and insert into quote body table
+    for i in range(len(quote_body)):
+        line_net_total = quote_body[i]["list_price"] * (1 - quote_body[i]["discounts"]) *  quote_body[i]["quantity"]
+        db.execute(
+            "INSERT INTO quote_body (quote_num, line_ref, item_id, quantity, list_price, discounts, line_net_total, lead_time) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", 
+            quote_number, quote_body[i]["line_ref"], quote_body[i]["item_id"], quote_body[i]["quantity"], 
+            quote_body[i]["list_price"], quote_body[i]["discounts"], line_net_total, quote_body[i]["lead_time"]
+        )
+    # Get the full total from body to update headers table
+    full_net_total = db.execute(
+        "SELECT SUM(line_net_total) FROM quote_body WHERE quote_num = ?", quote_number,
+    )
+    print(full_net_total)
+    db.execute(
+        "UPDATE quote_header SET total_net_value = ? WHERE quote_num = ?",
+        full_net_total[0]["SUM(line_net_total)"], quote_number
+    )
+    return jsonify({"message": "quote created"})
 
 
 
